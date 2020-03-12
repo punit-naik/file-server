9103e12b359e1dae73b87cca53255eb179c89dd1
Punit Naik <naik.punt44@gmail.com>
2020-03-12 17:23:27 +0530

    added a function which ignores trailing forward slashes in the route paths

--- src/file_server/handler.clj
+++ src/file_server/handler.clj
@@ -7,8 +7,23 @@
   [root-dir]
   (apply routes (generate-routes-for-dir root-dir)))
 
+(defn ignore-trailing-slash
+  "Modifies the request uri before calling the handler.
+  Removes a single trailing slash from the end of the uri if present.
+
+  Useful for handling optional trailing slashes until Compojure's route matching syntax supports regex.
+  Adapted from http://stackoverflow.com/questions/8380468/compojure-regex-for-matching-a-trailing-slash"
+  [handler]
+  (fn [request]
+    (let [uri (:uri request)]
+      (handler (assoc request :uri (if (and (not (= "/" uri))
+                                            (.endsWith uri "/"))
+                                     (subs uri 0 (dec (count uri)))
+                                     uri))))))
+
 (defn app
   "Handler for the server, accepts just one argument `serving-dir`, which is the directory path to be served"
   [serving-dir]
   (-> (dir-routes serving-dir)
+      ignore-trailing-slash
       (wrap-file serving-dir)))
