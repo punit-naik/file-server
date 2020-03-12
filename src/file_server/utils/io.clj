e9ff7038842ba98e5f5124aa62653985c5b07cd1
Punit Naik <naik.punt44@gmail.com>
2020-03-12 17:26:41 +0530

    Added IO utils with misc functions

---
+++ src/file_server/utils/io.clj
@@ -0,0 +1,24 @@
+(ns file-server.utils.io
+  (:require [clojure.string :refer [join split]]))
+
+(defn dir-not-empty?
+  [dir]
+  (> (count (.list dir)) 0))
+
+(defn sub-dir?
+  [dir]
+  (> (count (split dir #"/")) 1))
+
+(defn get-dir-without-root
+  "Gets the dir path as a string without the root directory
+   For e.g. If you provide `dir` as \"a/b/c\", this function will output \"b/c\""
+  [dir]
+  (->> (split dir #"\/")
+       rest
+       (join "/")))
+
+(defn get-free-port
+  []
+  (let [socket (java.net.ServerSocket. 0)]
+    (.close socket)
+    (.getLocalPort socket)))
